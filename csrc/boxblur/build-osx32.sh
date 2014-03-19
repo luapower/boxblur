66b040c85c59a92afcad06749e686f8793bd7a94
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:35:22 +0200

    osx32 & osx64 builds

---
+++ csrc/boxblur/build-osx32.sh
@@ -0,0 +1,1 @@
+gcc -arch i386 -O2 boxblur.c -shared -o ../../bin/osx32/libboxblur.dylib
--- csrc/boxblur/build-osx64.sh
+++ csrc/boxblur/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -O2 boxblur.c -shared -o ../../bin/osx64/libboxblur.dylib
+gcc -arch x86_64 -O2 boxblur.c -shared -o ../../bin/osx64/libboxblur.dylib
