55c1dffeb97fd01b87566d00f4697623526e94ff
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 00:00:20 +0200

    unimportant

--- csrc/boxblur/build-osx32.sh
+++ csrc/boxblur/build-osx32.sh
@@ -1,2 +1,2 @@
-P=osx32 C="-arch i386" L="-arch i386 -install_name @loader_path/libboxblur.dylib" \
+P=osx32 C="-arch i386" L="-arch i386 -install_name @rpath/libboxblur.dylib" \
 	D=libboxblur.dylib A=libboxblur.a ./build.sh
